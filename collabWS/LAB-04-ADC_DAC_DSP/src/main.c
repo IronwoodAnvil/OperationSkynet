ec2a68cafa210ecda2b03faa84ba8321639b5747
Patrick Love <patricklove@comcast.net>
2019-10-17 19:54:16 -0400

    Make more readable

--- collabWS/LAB-04-ADC_DAC_DSP/src/main.c
+++ collabWS/LAB-04-ADC_DAC_DSP/src/main.c
@@ -30,29 +30,34 @@ int main(void)
 	int32_t result;
 	asm("str r9, %0"
 			: "=m" (result));
-	printf("2718+31415 = %lu\r\n", result);
+	printf("%d+%d = %ld\r\n",2718,31415,result);
 
 	int16_t v1 = 2718;
 	int16_t v2 = 31415;
 	asm("smulbb %0, %1, %2"
 			: "=r" (result) : "r" (v1), "r" (v2));
-	printf("2718*31415 = %lu\r\n", result);
+	printf("%d*%d = %ld\r\n",v1,v2,result);
 
 	int32_t x = 42;
-	asm("mov %0, %1			\r\n" // x
-		"mul %0, %3			\r\n" // Multiply by 2
-		"sdiv %0, %2		\r\n" // Divide by 3
-		"add %0, #5			\r\n" // Add 5
-			: "=r" (result)
-			: "r" (x), "r" (3), "r" (2));
-	printf("[MUL] 42*2/3 + 5 = %lu\r\n", result);
+	asm("mov %[r], %[x]		\r\n" // x
+		"mul %[r], %[n2]	\r\n" // Multiply by 2
+		"sdiv %[r], %[n3]	\r\n" // Divide by 3
+		"add %[r], #5		\r\n" // Add 5
+			: [r] "=r" (result)
+			: [x] "r" (x), [n3] "r" (3), [n2] "r" (2));
+	printf("[MUL] %lu*2/3 + 5 = %lu\r\n", x, result);
 
-	asm("mov %0, #15		\r\n"
-		"mla %0, %1, %3, %0	\r\n" // 2*x + 15
-		"sdiv %0, %2		\r\n" // Divide by 3
-			: "=r" (result)
-			: "r" (x), "r" (3), "r" (2));
-	printf("[MAC] 42*2/3 + 5 = %lu\r\n", result);
+	result = 0; // Prove that actual result is regenerated
+	asm("mov %[r], #15					\r\n"
+		"mla %[r], %[x], %[n2], %[r]	\r\n" // 2*x + 15
+		"sdiv %[r], %[n3]				\r\n" // Divide by 3
+			: [r] "=r" (result)
+			: [x] "r" (x), [n3] "r" (3), [n2] "r" (2));
+	printf("[MAC] %ld*2/3 + 5 = %ld\r\n", x, result);
+
+	fflush(stdout);
+
+	while(1);
 
 #elif SUB_TASK == 2
 
