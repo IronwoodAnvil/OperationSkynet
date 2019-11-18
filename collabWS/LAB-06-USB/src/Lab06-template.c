a7847f33bd7636bfbe260c1fc73c3b0929fafb12
Nathan Mascari <nate.mascari@gmail.com>
2019-11-18 16:32:27 -0500

    added basic stuff from manual

--- collabWS/LAB-06-USB/src/Lab06-template.c
+++ collabWS/LAB-06-USB/src/Lab06-template.c
@@ -16,15 +16,38 @@ USBH_HandleTypeDef husbh;
 void USBH_UserProcess(USBH_HandleTypeDef *, uint8_t);
 
 
+/*Please note that the application can register multiple classes, for example:
+ Add Supported Class: example HID and MSC classes
+ USBH_RegisterClass(&hUSBHost, USBH_HID_CLASS);
+ USBH_RegisterClass(&hUSBHost, USBH_MSC_CLASS);
+The user application can determine the enumerated class using core API function
+USBH_GetActiveClass() when HOST_USER_CLASS_ACTIVE event occurs.*/
+
 
 int main(void){
 	 // System Initializations
 	Sys_Init();
 
+
+
+
+
+	/* Start Host Process */
 	// Application Initializations
+
+
+
 	// USBH Driver Initialization
 	// USB Driver Class Registrations: Add device types to handle.
+
+	/* Init Host Library */
+	USBH_Init(&hUSBHost, USBH_UserProcess, 0);
+
+	/* Add Supported Class: example HID class*/
+	USBH_RegisterClass(&hUSBHost, USBH_HID_CLASS);
+
 	// Start USBH Driver
+	USBH_Start(&hUSBHost);
 
 	while(1){
 		USBH_Process(&husbh);
@@ -33,7 +56,27 @@ int main(void){
 }
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
-	// ...
+	/*The USBH_UserProcess callback handles theUSB host core events (for example
+disconnection, connection, class active). Typically, it should be implemented as shown
+hereafter to handle application process state machine:*/
+	switch (id)
+	 {
+	 case HOST_USER_DISCONNECTION:
+	 Appli_state = APPLICATION_DISCONNECT;
+	 break;
+
+	/* when HOST_USER_CLASS_ACTIVE event is received, application can start
+	 communication with device*/
+	 case HOST_USER_CLASS_ACTIVE:
+	 Appli_state = APPLICATION_READY;
+	 break;
+
+	 case HOST_USER_CONNECTION:
+	 Appli_state = APPLICATION_START;
+	 break;
+	default:
+	 break;
+	 }
 }
 
 // Interrupts and Callbacks...
